2ead0d77697a3231b6bfdb4ad87cf19772725abf
RachData <rachidderra@gmail.com>
2020-05-02 16:42:46 +0200

    Implementation d'un cercle

--- Exercice9_9/src/main/java/fr/uvsq/pglp/Exercice9_9/Cercle2D.java
+++ Exercice9_9/src/main/java/fr/uvsq/pglp/Exercice9_9/Cercle2D.java
@@ -1,7 +1,52 @@
 package fr.uvsq.pglp.Exercice9_9;
 
+/**
+ * un cercle
+ * @author root
+ *
+ */
+
 public class Cercle2D {
 	
+	private Point2D center;
+	private double rayon;
+
+	/**
+	 * Initialisation du cerle et de son centre.
+	 * 
+	 * @param center the center
+	 * @param radius the radius
+	 */
+	public Cercle2D(Point2D center, double radius) {
+		this.center = center;
+		this.rayon = radius;
+	}
+
 	
+	/**
+	 * retourn le rayon du cercle.
+	 * 
+	 * @return le rayon
+	 */
+	public double getRadius() {
+		return rayon;
+	}
+
+
+	/**
+	 * donne la description d'un cercle.
+	 * 
+	 * @return en chaine de caractere la description
+	 */
+	@Override
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append('[');
+		str.append(center.toString());
+		str.append(", ");
+		str.append(rayon);
+		str.append(']');
+		return str.toString();
+	}
 
 }
--- Exercice9_9/src/main/java/fr/uvsq/pglp/Exercice9_9/Point2D.java
+++ Exercice9_9/src/main/java/fr/uvsq/pglp/Exercice9_9/Point2D.java
@@ -1,5 +1,54 @@
 package fr.uvsq.pglp.Exercice9_9;
 
 public class Point2D {
+	
+	private double x;
+	private double y;
+
+	/**
+	 * Initialize from 2 coordinates.
+	 * 
+	 * @param x x coordonner
+	 * @param y y coordonner
+	 */
+	public Point2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * retourne la coordonnée X.
+	 * 
+	 * @return X coordinate
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * retourne la coordonnée Y.
+	 *
+	 * @return Y coordinate
+	 */
+	public double getY() {
+		return y;
+	}
+
+
+	/**
+	 * donne la description d'un Point.
+	 * 
+	 * @return en chaine de caractere la description
+	 */
+    @Override
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append('(');
+		str.append(x);
+		str.append(", ");
+		str.append(y);
+		str.append(')');
+		return str.toString();
+	}
 
 }
