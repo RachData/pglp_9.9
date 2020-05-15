89825fcd6faa99d4e3cdf3f915f85b6113b33f2b
RachData <rachidderra@gmail.com>
2020-05-15 19:19:11 +0200

    fonction d'affichage

---
+++ Exercice9_9/src/main/java/fr/uvsq/pglp/Exercice9_9/Allfigure/AfficheFig.java
@@ -0,0 +1,23 @@
+package fr.uvsq.pglp.Exercice9_9.Allfigure;
+
+/**
+ * Affichage
+ * @author root
+ *
+ */
+public class AfficheFig {
+
+	/**
+	 * Afiche une figure ou un groupe de figure
+	 * @param allfig la ou les grouprs a afficher
+	 */
+	public static void affich (allfigure allfig) {
+		ItteratorFigure affich = new ItteratorFigure(allfig);
+		Iterator grouptIter = affich.getIterator();
+		while (grouptIter.HasNext()) {
+			allfigure nextValue=grouptIter.Next();
+			System.out.println("The next value with Iterator is: " + nextValue.toString());
+		}
+
+	}
+}
